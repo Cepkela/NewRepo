14fea65b86a1ff54520506c32f489f1f129e7036
Ruslan <s035180@ad.viko.lt>
2020-06-12 15:49:28 +0300

    asdasdasdasda

--- 4_Galutinis_Darbas/4_Galutinis_Darbas/Program.cs
+++ 4_Galutinis_Darbas/4_Galutinis_Darbas/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace _4_Galutinis_Darbas
 {
     //asdasdasdsad
+    //asdasdasdasd
     public class Program
     {
         public static void Main(string[] args)
