0687e874a82a4febe61f8fd03a0788b489ca3bc0
Ruslan <s035180@ad.viko.lt>
2020-06-13 14:43:53 +0300

    Not wroking

--- 4_Galutinis_Darbas/4_Galutinis_Darbas/Controller/PathController.cs
+++ 4_Galutinis_Darbas/4_Galutinis_Darbas/Controller/PathController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _4_Galutinis_Darbas.Models;
+using _4_Galutinis_Darbas.Rep;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
@@ -23,11 +25,11 @@ namespace _4_Galutinis_Darbas.Controller
         // GET: api/Path/5
         public JObject Get(string pav, string pav2)
         {
-            var client = new RestClient("https://www.mapquestapi.com/directions/v2/route?key=KEY&from=" + pav + "&to=" + pav2 + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false");
-            var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
-            JObject json = JObject.Parse(response.Content);
-            return json;
+            PlaceRep pr = new PlaceRep();
+            JObject j = new JObject();
+            j = pr.Action(pav, pav2);
+            return j;
+
         }
 
         // POST: api/Path
